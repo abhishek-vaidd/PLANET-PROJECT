0b58596666d27c868d7777178ecbef2866811514
Abhishek-vaid-STC <abhishek.vaid@seventhtriangle.com>
2023-02-04 01:08:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const parse = require('csv-parser')
 const fs = require('fs')
-const results = [];
+const HabitablePlanets = [];
+const isHabitablePlanets = (planet) => {
+    return planet["koi_disposition"] === "CONFIRMED" 
+    && planet["koi_insol"] > 0.36 && planet["koi_insol"] < 1.11  
+    && planet["koi_prad"] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({
     skipComments: "#",
     columns: true,
   }))
-  .on('data', (data) => results.push(data))
+  .on('data', (data) => {
+    if(isHabitablePlanets(data)) HabitablePlanets.push(data)
+  })
   .on('end', () => {
-    console.log(results);
+    console.log(HabitablePlanets.length);
     console.log('done')
   });
